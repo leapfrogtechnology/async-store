5af9ad8457bd6632d805621fb7466f15957bd9a7
silwalanish <anishsilwal@lftechnology.com>
2019-09-23 11:27:26 +0545

    Add debug method to log debug and Use short request id if available

--- examples/express-ts/src/logger.ts
+++ examples/express-ts/src/logger.ts
@@ -1,12 +1,32 @@
 import * as store from '@leapfrogtechnology/async-store';
 
 /**
+ * Get unique request id from store or return empty string
+ *
+ * @returns {string}
+ */
+function getRequestId() {
+  return (store.getId() || '').substring(0, 8);
+}
+
+/**
  * A example service to log info with unique request id
  *
  * @param {string} infoText
  */
 export function info(infoText: string) {
-  const requestId = store.getId();
+  const requestId = getRequestId();
 
-  process.stdout.write(`[ INFO ] [ ${requestId} ] ${infoText}`);
+  process.stdout.write(`[ INFO ] ${requestId ? `[ ${requestId} ]` : ''} ${infoText}\n`);
+}
+
+/**
+ * A example service to log debug with unique request id
+ *
+ * @param {string} debugText
+ */
+export function debug(debugText: string) {
+  const requestId = getRequestId();
+
+  process.stdout.write(`[ DEBUG ] ${requestId ? `[ ${requestId} ]` : ''} ${debugText}\n`);
 }
