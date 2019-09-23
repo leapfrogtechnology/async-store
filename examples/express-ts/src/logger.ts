99b7cdf34062f54a157e4fed96b3d802c3633154
silwalanish <anishsilwal@lftechnology.com>
2019-09-23 10:28:59 +0545

    Add type for parameter in docblock

--- examples/express-ts/src/logger.ts
+++ examples/express-ts/src/logger.ts
@@ -3,7 +3,7 @@ import * as store from '@leapfrogtechnology/async-store';
 /**
  * A example service to log info with unique request id
  *
- * @param infoText Text to log
+ * @param {string} infoText
  */
 export function info(infoText: string) {
   const requestId = store.getId();
