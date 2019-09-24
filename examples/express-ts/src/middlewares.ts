b7164aca5431d9b7ee572aadb9a3c9edb314afd6
silwalanish <anishsilwal@lftechnology.com>
2019-09-24 12:47:01 +0545

    Remove unnecessary middlewares

--- examples/express-ts/src/middlewares.ts
+++ examples/express-ts/src/middlewares.ts
@@ -2,6 +2,7 @@ import * as store from '@leapfrogtechnology/async-store';
 import { Request, Response, NextFunction } from 'express';
 
 import * as logger from './logger';
+import { doSomething } from './service';
 
 /**
  * Middleware to set query params `a` and `b` on async-store.
@@ -31,8 +32,9 @@ export function requestParams() {
  * @returns {(req, res, next) => void}
  */
 export function add() {
-  return (req: Request, res: Response, next: NextFunction) => {
-    logger.info('Simulating Delay');
+  return async (req: Request, res: Response, next: NextFunction) => {
+    await doSomething();
+
     const a = store.get('a');
     const b = store.get('b');
 
@@ -43,30 +45,4 @@ export function add() {
 
     next();
   };
-}
-
-/**
- * Middleware to set the request context `x-id` on the async store.
- *
- * @returns {(req, res, next) => void}
- */
-export function requestContext() {
-  return (req: Request, res: Response, next: NextFunction) => {
-    store.set({ 'x-id': req.header('x-id') });
-
-    next();
-  };
-}
-
-/**
- * Middleware where the value set in the store is available for use.
- *
- * @returns {(req, res, next) => void}
- */
-export function otherMiddleware(req: Request, res: Response, next: NextFunction) {
-  const requestId = store.get('x-id');
-
-  logger.info(`X-Id received in the middleware: ${requestId}\n`);
-
-  next();
 }
