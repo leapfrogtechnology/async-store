c799c664bcbe2ae8ffe3217dabcdf5a36c56a113
silwalanish <anishsilwal@lftechnology.com>
2019-09-27 13:46:48 +0545

    Add routing service

---
+++ examples/http-ts/src/router.ts
@@ -0,0 +1,31 @@
+import { URL } from 'url';
+import { IncomingMessage, ServerResponse } from 'http';
+
+import error404 from './error404';
+import * as logger from './logger';
+import { toFullURL } from './utils';
+import queryToStore from './queryToStore';
+import routes, { Controller } from './routes';
+
+/**
+ * Main application router
+ *
+ * @param { IncomingMessage } req
+ * @param { ServerResponse } res
+ */
+export default function(req: IncomingMessage, res: ServerResponse): void {
+  const url: URL = toFullURL(req.url || '');
+  const controller: Controller | null = routes(url.pathname);
+
+  logger.info(`Received a request at url: ${url.pathname}`);
+
+  if (!controller) {
+    error404(req, res);
+
+    return;
+  }
+
+  queryToStore(url.searchParams);
+
+  controller(req, res);
+}
---
+++ examples/http-ts/src/routes.ts
@@ -0,0 +1,30 @@
+import { IncomingMessage, ServerResponse } from 'http';
+
+import * as controller from './controller';
+
+const HOME_ROUTE = '/home';
+const OTHER_ROUTE = '/other';
+
+export type Controller = (req: IncomingMessage, res: ServerResponse) => void;
+
+/**
+ * Returns a controller for a path if exists, null otherwise.
+ *
+ * @param {string} path A path to find controller for.
+ *
+ * @returns {Controller | null} Controller for path.
+ */
+export default function(path: string | undefined): Controller | null {
+  if (!path) {
+    return null;
+  }
+
+  switch (path.toLowerCase()) {
+    case HOME_ROUTE:
+      return controller.home;
+    case OTHER_ROUTE:
+      return controller.other;
+    default:
+      return null;
+  }
+}
