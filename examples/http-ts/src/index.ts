88ac57c4baaa9a0dd75af0ab352b0b7a2df969e9
silwalanish <anishsilwal@lftechnology.com>
2019-09-27 17:20:48 +0545

    Simplify example

--- examples/http-ts/src/index.ts
+++ examples/http-ts/src/index.ts
@@ -2,9 +2,26 @@ import * as http from 'http';
 import * as store from '@leapfrogtechnology/async-store';
 import AsyncStoreAdapter from '@leapfrogtechnology/async-store/dist/AsyncStoreAdapter';
 
-import router from './router';
 import * as logger from './logger';
-import { BASE_URL, PORT } from './config';
+import { requestParams, add } from './middlewares';
+
+export const PORT = process.env.PORT || 8000;
+export const BASE_URL = `http://localhost:${PORT}`;
+
+/**
+ * Main application router
+ *
+ * @param {IncomingMessage} req
+ * @param {ServerResponse} res
+ */
+function router(req: http.IncomingMessage, res: http.ServerResponse) {
+  const params = (req.url || '').split('?', 2)[1];
+
+  store.set({ query: params });
+
+  requestParams(req, res);
+  add(req, res);
+}
 
 const app = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
   const init = store.initialize(AsyncStoreAdapter.DOMAIN);
