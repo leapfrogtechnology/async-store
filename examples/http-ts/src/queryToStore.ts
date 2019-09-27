12718f55c16dd0e678524befee82cdc312ff64cd
silwalanish <anishsilwal@lftechnology.com>
2019-09-27 16:13:33 +0545

    Have concise name for the identifier

--- examples/http-ts/src/queryToStore.ts
+++ examples/http-ts/src/queryToStore.ts
@@ -4,16 +4,16 @@ import * as store from '@leapfrogtechnology/async-store';
 /**
  * Get the query params from the url and set them to async-store with `query` key.
  *
- * @param {URLSearchParams} url
+ * @param {URLSearchParams} params
  */
-export default function queryToStore(urlQueryParams: URLSearchParams) {
-  const queryKeys: Iterator<string> = urlQueryParams.keys();
+export default function queryToStore(params: URLSearchParams) {
+  const queryKeys: Iterator<string> = params.keys();
   const queryParams: any = {};
 
   let result: IteratorResult<string, any> = queryKeys.next();
   while (!result.done) {
     const key: string = result.value;
-    const value: string[] = urlQueryParams.getAll(key);
+    const value: string[] = params.getAll(key);
 
     queryParams[key] = value.length === 1 ? value[0] : value;
 
