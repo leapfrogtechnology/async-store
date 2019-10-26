878db7edfc7ae25aebeb9ffcb91ce5c83a52ecff
Swechhya <swechhyabista@lftechnology.com>
2019-10-26 14:32:44 +0545

    Add example project for koa

---
+++ examples/koa-http-server-js/backpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  webpack: config => {
+    config.context = path.resolve(__dirname, 'src');
+    config.entry = {
+      main: './index.js'
+    };
+    config.output.path = path.resolve(__dirname, 'dist');
+    config.resolve.extensions = ['.js', '.json'];
+
+    // When we build with webpack it messes up the global '__dirname'
+    // given by node. To undo this we have to set this configuration
+    // https://github.com/webpack/webpack/issues/1599
+    config.node = {
+      __dirname: false
+    };
+
+    return config;
+  }
+};
