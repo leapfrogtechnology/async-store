8a0ade91a53add0be2bbd656b4c95cb5b4d323d8
sarojrana <sarojrana@lftechnology.com>
2019-11-27 22:33:29 +0545

    Add async-store example for koajs using typescript.

---
+++ examples/koa-http-server-ts/src/service.ts
@@ -0,0 +1,37 @@
+import * as qs from 'qs';
+import * as store from '@leapfrogtechnology/async-store';
+
+import * as logger from './logger';
+
+/**
+ * Set input params received from query in the store.
+ *
+ * @param {any} query
+ */
+export function storeParams(query: any) {
+  const { a, b } = qs.parse(query);
+
+  store.set({ a, b });
+
+  logger.debug(`Persisted a: ${a}`);
+  logger.debug(`Persisted b: ${b}`);
+}
+
+/**
+ * An example function making use of the request context
+ * set in the store asynchronously (with delay).
+ *
+ * @returns {Promise<any>}
+ */
+export function doSomethingAsync(): Promise<any> {
+  // Do something with the request with a delay.
+  logger.debug('Simulating delayed access');
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = store.getAll();
+      logger.info('Store contents: ' + JSON.stringify(data));
+      resolve();
+    }, 2000);
+  });
+}
