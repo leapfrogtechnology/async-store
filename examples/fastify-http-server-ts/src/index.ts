b728bbe5be25424b7f99ccd8328d2b5ecf1656a5
prabeshpd <prabesh1995nepalktm@gmail.com>
2022-01-11 11:54:38 +0545

    Add example for fastify to initialize the async store through plugin

---
+++ examples/fastify-http-server-ts/src/index.ts
@@ -0,0 +1,37 @@
+import * as store from '@leapfrogtechnology/async-store';
+import fastifyPlugin from 'fastify-plugin';
+import Fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+
+import { storeParamsPlugin, calculateSum } from './plugin';
+
+const fastifyServer: FastifyInstance = Fastify({ logger: true });
+
+fastifyServer.register(fastifyPlugin(store.initializePlugin()));
+fastifyServer.register(fastifyPlugin(storeParamsPlugin));
+
+fastifyServer.register((fastifyInstance, opts, done) => {
+  fastifyInstance.register(fastifyPlugin(calculateSum));
+  fastifyInstance.get('/', (req: FastifyRequest, reply: FastifyReply) => {
+    const a = store.get('a');
+    const b = store.get('b');
+    const sum = store.get('sum');
+
+    reply.send(`Sum of ${a}, ${b}: ${sum}\n`);
+
+    reply.log.info('Response Sent');
+  });
+
+  done();
+});
+
+const start = async () => {
+  try {
+    await fastifyServer.listen(3000);
+    fastifyServer.log.info(`Server is listening at 3000`);
+  } catch (err) {
+    fastifyServer.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
---
+++ examples/fastify-http-server-ts/src/plugin.ts
@@ -0,0 +1,48 @@
+import * as store from '@leapfrogtechnology/async-store';
+// import fastifyPlugin from 'fastify-plugin';
+import {
+  FastifyReply,
+  FastifyRequest,
+  FastifyPluginCallback,
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyPluginAsync
+} from 'fastify';
+import { doSomethingAsync } from './service';
+
+type RequestQuery = FastifyRequest<{
+  Querystring: { a: string; b: string };
+}>;
+
+export const storeParamsPlugin: FastifyPluginCallback = (
+  fastify: FastifyInstance,
+  opts: FastifyPluginOptions,
+  next: any
+) => {
+  fastify.addHook('onRequest', (req: RequestQuery, reply: FastifyReply, done) => {
+    const { a, b } = req.query;
+    store.set({ a, b });
+
+    fastify.log.debug(`Persisted a: ${a}`);
+    fastify.log.debug(`Persisted b: ${b}`);
+
+    done();
+  });
+
+  next();
+};
+
+export const calculateSum: FastifyPluginAsync = async (fastify: FastifyInstance, opts: FastifyPluginOptions) => {
+  fastify.addHook('preHandler', async (req, reply) => {
+    const data = await doSomethingAsync();
+    fastify.log.info(`Store contents: ${JSON.stringify(data)}`);
+    const a = +store.get('a');
+    const b = +store.get('b');
+    const sum = a + b;
+
+    store.set({ sum });
+
+    fastify.log.debug(`Calculated sum: ${sum}`);
+    fastify.log.debug(`Persisted sum: ${sum}`);
+  });
+};
